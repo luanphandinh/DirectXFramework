23d4b3db2a80d2cc47b730f74d8e45556c4f0d7e
tinifyphan <tinify.phan@gmail.com>
2016-10-26 14:42:13 +0700

    Update gameTime

--- FrameWork/GameTime.cpp
+++ FrameWork/GameTime.cpp
@@ -1,11 +1,91 @@
-#include "GameTime.h"
+﻿#include "GameTime.h"
+_USING_FRAMEWORK
 
-
-GameTime::GameTime()
-{
-}
-
+GameTime* GameTime::_instance = nullptr;
 
 GameTime::~GameTime()
 {
+}
+
+GameTime::GameTime()
+{
+
+}
+
+GameTime::GameTime(TimeSpan& elapsedGameTime)
+{
+	this->_elapsedGameTime = elapsedGameTime;
+}
+
+void GameTime::setELapsedGameTime(TimeSpan& elapsedGameTime)
+{
+	this->getElapsedGameTime = elapsedGameTime;
+}
+
+void GameTime::setTotalGameTime(TimeSpan& TotalGameTime)
+{
+	this->_totalGameTime = TotalGameTime;
+}
+
+void GameTime::release()
+{
+	delete _instance;
+	_instance = NULL;
+}
+
+GameTime* GameTime::getInstance()
+{
+	if (_instance == NULL)
+		_instance = new GameTime();
+	return _instance;
+}
+
+void GameTime::GameTime::init()
+{
+	//lấy số xung nhịp cpu/giây
+	QueryPerformanceFrequency(&this->_Query);
+	//lưu vào _freQuery
+	this->_freQuery = (float)_Query.QuadPart / TimeSpan::TicksPerSecond;
+	// hàm QueryPerformanceCounter(LARGE_INTEGER *) để thực hiện việc đo thời gian
+	// Hàm này có kiểu trả về là BOOL và trả về giá trị là thời gian tính bằng giây từ lúc chương trình bắt đầu chạy,
+	//tham số đầu vào là con trỏ tới một biến kiểu LARGE_INTEGER.
+	//ở đây giá trị LARGE_INTEGER là _Query 
+	QueryPerformanceCounter(&_Query);
+	//gán startTIcks và lastTicks bằng thời gian khi init();
+	startTicks = lastTicks = _Query.QuadPart;
+	_totalGameTime = (TimeSpan)0;
+}
+
+void GameTime::resetLastTick()
+{
+	lastTicks = 0;
+	curTicks = 0;
+	_totalGameTime = (TimeSpan)0;
+}
+//tính time 
+//http://diendan.congdongcviet.com/threads/t210307::huong-dan-viet-mot-game-don-gian-trong-directx-tu-a-z.cpp
+void GameTime::updateGameTime()
+{
+	QueryPerformanceCounter(&_Query);
+	curTicks = _Query.QuadPart;
+	//16 = 1000/60 = frame_rate
+	if ((UINT64)((float)(curTicks - lastTicks) / _freQuery) < TimeSpan::TicksPerMilisecond * 16)
+	{
+		return;
+	}
+	auto gt = ((float)(curTicks - lastTicks)) / _freQuery;
+	this->setTotalGameTime(_totalGameTime + gt);
+	this->setELapsedGameTime(TimeSpan((UINT64)gt));
+	lastTicks = curTicks;
+}
+//Tổng thời gian trôi qua kể từ lần cuối gọi update
+float GameTime::getElapsedGameTime()
+{
+	return this->_elapsedGameTime.getMiliSeconds();
+}	
+
+//Tổng thời gian trôi qua kể từ lần cuối gọi init
+float GameTime::getTotalGameTime()
+{
+	return this->_totalGameTime.getMiliSeconds();
 }
--- FrameWork/GameTime.h
+++ FrameWork/GameTime.h
@@ -1,9 +1,41 @@
-#pragma once
+﻿#pragma once
 #include "define.h"
+#include "TimeSpan.h"
+_NAMESPACE_FRAMEWORK_BEGIN
+///Tương tự graphics
+///GameTime sử dụng 1 static member getInstance()
+///để lấy một instance thông qua tên class mà có thể sử dụng ở bất kì đâu sau 
+///lời gọi getInstance()->Init() trong mainGame
 class GameTime
 {
 public:
-	GameTime();
 	~GameTime();
+	static void release();
+	static GameTime* getInstance();
+	void init();
+	void resetLastTick();
+
+	void updateGameTime();
+
+	float getElapsedGameTime();	//Tổng thời gian trôi qua kể từ lần cuối gọi update
+	float getTotalGameTime();	//Tổng thời gian trôi qua kể từ lần cuối gọi init
+private:
+	static GameTime* _instance;
+
+	TimeSpan	_elapsedGameTime;
+	TimeSpan	_totalGameTime;
+	LARGE_INTEGER	_Query;//Dùng làm tham số cho hàm QueryPerformanceCounter()
+	float			_freQuery;
+
+	LONGLONG	startTicks;
+	LONGLONG	lastTicks;
+	LONGLONG	curTicks;
+
+	GameTime();
+	GameTime(TimeSpan& elapsedGameTime);
+
+	void setELapsedGameTime(TimeSpan& elapsedGameTime);
+	void setTotalGameTime(TimeSpan& TotalGameTime);
 };
+_NAMESPACE_FRAMEWORK_END
 
