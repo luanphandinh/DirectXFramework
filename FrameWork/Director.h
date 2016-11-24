73d381060d59e5a89517ff647948ca5b38e65475
tinifyphan <tinify.phan@gmail.com>
2016-11-24 16:28:24 +0700

    ...

--- FrameWork/Director.h
+++ FrameWork/Director.h
@@ -3,14 +3,6 @@
 /*
 	Class đùng để gán phần viewport cho từng đoạn chơi trong stage
 	Được đọc từ file trong Resources//Maps//level?viewportinfo.txt
-	map<string,RECT> string là tên đoạn
-					RECT.left là pos.x của viewport(x trong file được đọc lên)
-					RECT.top là pos.y của viewport
-					Khi chuyển scene thì 2 cái này là pos của viewport trong world
-
-					RECT.right biên bên trái của viewport(là size.x của vùng đi được trong viewport
-					hiện tại cho đến khi qua cửa hoặc lên cầu thang đến đoạn khác)
-					RECT.bottm là biên phải của viewport
 */
 struct AreaBound
 {
